2fe39278e8c3136f21109697512f4d16350523d4
evie15 <121596390+evie15@users.noreply.github.com>
2023-10-10 10:35:19 +0800

    ppp

--- PharmDataProject/DatatoMongo/KEGGtoMongo.py
+++ PharmDataProject/DatatoMongo/KEGGtoMongo.py
@@ -1,61 +1,37 @@
-"""
-  -*- encoding: utf-8 -*-
-  @Author: evie
-  @Time  : ${DATA}
-  @Email: 2762376919@qq.com
-  @function
-"""
 import pymongo
 import json
 
-import requests
 from Bio.KEGG import REST
 from pymongo.collection import Collection
 from pymongo.database import Database
-import configparser
-from PharmDataProject.Utilities.Database.dbutils import DBconnection
-import os
-import json
-from pymongo import MongoClient
 
-class KeggtoMongo:
-    def __init__(self, folder_path, db):
-        self.folder_path = folder_path
+from PharmData.PharmDataProject.DataParsers.KEGGParsers import Parse
+from PharmData.PharmDataProject.DataSources.getId import GetId
 
-        self.db = db
-        self.collection = self.db.collection
 
-    def read_json_file(self, file_path):
-        with open(file_path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-        return data
+class dataSave:
+    def save(info,db_name,host,port,collection_name, username, password):
+        # username = "user"
+        # password = "1234567890"
 
-    def insert_into_mongodb(self, data):
-        self.collection.insert_one(data)
+        client = pymongo.MongoClient(f"mongodb://{username}:{password}@{host}:{port}", connectTimeoutMS=90000)
+        db = client[db_name]
 
-    def process_json_files(self):
-        for root, dirs, files in os.walk(self.folder_path):
-            for file_name in files:
-                if file_name.endswith('.json'):
-                    file_path = os.path.join(root, file_name)
-                    json_data = self.read_json_file(file_path)
-                    self.insert_into_mongodb(json_data)
-                    print(f'Inserted data from {file_path} into MongoDB')
-
+        collection = db[collection_name]
+        collection.insert_many(info)
 
 if __name__ == "__main__":
+    # with open('../../json/H01476.json') as f:
+    #     data=json.load(f)
+    # dataSave.save(data,"PharmRG","117.73.10.251",27017,"KEGG_Disease","readwrite","readwrite")
+    items = GetId.get_id("disease")
+    for item in items:
+        print(item)
+        record = REST.kegg_get(item)
+        data = record.read()
+        entries = data.strip().split("\n///\n")
+        #     # 装到一个JSON格式
+        json_data = Parse.convert_to_json(entries)
 
-
-    config = configparser.ConfigParser()
-    cfgfile = '../conf/drugkb_test.config'
-    config.read(cfgfile)
-    for i in range(2, int(config.get('kegg', 'data_path_num'))-2):
-        db = DBconnection(cfgfile, config.get('kegg', 'db_name'),
-                          config.get('kegg', 'col_name_' + str(i + 1)))
-        # database_name = config.get('kegg', 'source_url_' + str(i + 1))
-        json_data_path = config.get('kegg','data_path_'+str(i+1))
-
-        json_processor = KeggtoMongo(json_data_path, db)
-        json_processor.process_json_files()
-
+        dataSave.save(json_data, "PharmRG", "59.73.198.168", 27017, "KEGG_Disease", "readwrite", "readwrite")
 
