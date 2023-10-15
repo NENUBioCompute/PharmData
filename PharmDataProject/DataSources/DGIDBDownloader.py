a80a1145aa5033f5b44929dac38eb4ce23a9d90a
jingtong <2665109868@qq.com>
2023-10-15 19:14:40 +0800

    database Parser

--- PharmDataProject/DataSources/DGIDBDownloader.py
+++ PharmDataProject/DataSources/DGIDBDownloader.py
@@ -1,33 +1,52 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2021/6/23
+@Author  : Jean Qu
+@Email   : quj165@nenu.edu.cn
+@File    : get_data.py
+@Software: PyCharm
+
+"""
+
 import wget
 import os
 import configparser
 
-class DGDBDownloader:
-    def __init__(self):
-        pass
 
-    def mkdir(self, path):
-        # 检查路径是否存在，如果不存在则创建
-        if not os.path.exists(path):
-            os.makedirs(path)
+def mkdir(path):
+    isExists = os.path.exists(path)
+    if not isExists:
+        os.makedirs(path)
+        return path
+    else:
         return path
 
-    def download_to_data(self, url, path):
-        # 下载文件到指定路径
-        print(path.split('/')[-2], "正在下载...")
-        obj = os.path.join(self.mkdir(path), url.split('/')[-1])
-        wget.download(url, obj)
-        print("下载完成！")
+def download_to_data(url, path):
+    print(path.split('/')[-2], "正在下载...")
+
+    obj = os.path.join(mkdir(path), url.split('/')[-1])
+    wget.download(url, obj)
+
+    print(" 下载完成！")
+
 
 if __name__ == '__main__':
+    # d = os.path.dirname(os.path.abspath(__file__))
+
     config = configparser.ConfigParser()
     cfgfile = '../conf/drugkb.config'
     config.read(cfgfile)
 
-    downloader = DGDBDownloader()  # 实例化DGDBDownloader类
+    download_to_data(config.get('dgidb', 'source_url_1'),
+                 config.get('dgidb', 'data_path_1'))
 
-    # 通过实例调用方法
-    downloader.download_to_data(config.get('dgidb', 'source_url_1'), config.get('dgidb', 'data_path_1'))
-    downloader.download_to_data(config.get('dgidb', 'source_url_2'), config.get('dgidb', 'data_path_2'))
-    downloader.download_to_data(config.get('dgidb', 'source_url_3'), config.get('dgidb', 'data_path_3'))
-    downloader.download_to_data(config.get('dgidb', 'source_url_4'), config.get('dgidb', 'data_path_4'))
+    download_to_data(config.get('dgidb', 'source_url_2'),
+                 config.get('dgidb', 'data_path_2'))
+
+    download_to_data(config.get('dgidb', 'source_url_3'),
+                 config.get('dgidb', 'data_path_3'))
+
+    download_to_data(config.get('dgidb', 'source_url_4'),
+                 config.get('dgidb', 'data_path_4'))
+
