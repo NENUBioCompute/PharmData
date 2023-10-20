3fed99c45ebef146241a73d2cf722e9865de8535
jingtong <2665109868@qq.com>
2023-10-20 20:27:45 +0800

    pharmrgkb downloader

--- PharmDataProject/DataSources/PharmRGKBDownloader.py
+++ PharmDataProject/DataSources/PharmRGKBDownloader.py
@@ -1,32 +1,36 @@
+# Title     : myGetData.py.py
+# Created by: julse@qq.com
+# Created on: 2021/5/28 17:06
+# des : download zip data and unzip data
 import configparser
 import os
 
 
-class PharmRGKBDownloader:
-    def __init__(self):
-        pass
 
-    def download(self, url, dir):
-        # 使用wget下载文件
-        os.system('wget %s -O %s' % (url, dir))
+def download(url,dir):
+    os.system('wget %s -O %s'%(url,dir))
 
-    def un_gz(self, file_name):
-        # 解压zip文件
-        f = file_name.replace(".zip", "")
-        os.system('unzip %s -d %s' % (file_name, f))
+def un_gz(file_name):
+    f = file_name.replace(".zip", "")
+    os.system('unzip %s -d %s'%(file_name,f))
 
 def main(url, dir):
     print('start download')
-    downloader = PharmRGKBDownloader()  # 创建类的实例
-    downloader.download(url, dir)  # 通过实例调用download方法
-    downloader.un_gz(dir)  # 通过实例调用un_gz方法
+    download(url,dir)
+    un_gz(dir)
 
+#########################################################################################################################################
 if __name__ == '__main__':
+
     config = configparser.ConfigParser()
     cfgfile = '../conf/drugkb.config'
     config.read(cfgfile)
-    # 下载和解压源数据
-    for idx in range(1, int(config.get('pharmgkb', 'col_num')) + 1):
-        source_url = config.get('pharmgkb', 'source_url_%d' % idx)
-        data_path = config.get('pharmgkb', 'data_path_%d' % idx)
-        main(source_url, data_path)
+    '''
+    download and extract source data
+    '''
+    for idx in range(1,int(config.get('pharmgkb', 'col_num'))+1):
+        source_dir = config.get('pharmgkb', 'source_url_%d'%idx)
+        data_dir = config.get('pharmgkb', 'data_path_%d'%idx)
+        main(source_dir,data_dir)
+
+
--- PharmDataProject/Utilities/NetDownloads/HttpDownloader.py
+++ PharmDataProject/Utilities/NetDownloads/HttpDownloader.py
@@ -9,31 +9,19 @@ import logging
 import requests
 import subprocess
 from multiprocessing import Process
-from threading import Thread
 from urllib import request, error
 from PharmDataProject.Utilities.FileDealers.FileSystem import *
-import wget
-
 
 class HTTP:
     """
     Downloading specific data source files with responding url in config file.
     """
-
-    @staticmethod
-    def download(url: str, local_path: str, file_name: str, ):
-        p1 = Process(target=HTTP.get_data, args=(url, local_path, file_name), name=file_name)
+    def DownLoad( url:str, local_path:str, file_name:str):
+        p1 = Process(target=HTTP.GetData, args=(url, local_path, file_name),name=file_name)
         p1.start()
         return os.getpid()
 
-    @staticmethod
-    def download_with_thread(url: str, local_path: str, file_name: str, block: bool = True):
-        thread = Thread(target=HTTP.get_data, args=(url, local_path, file_name), name=file_name)
-        thread.start()
-        if block:
-            thread.join()
-
-    def get_data(url: str, local_path: str, file_name: str):
+    def GetData( url:str, local_path:str, file_name:str):
         log_path = './test.log'
         logger = logging.getLogger('HttpDownloader')
         logger.setLevel(logging.INFO)
@@ -47,9 +35,7 @@ class HTTP:
             start = time.time()
             folder_is_exists.__func__(local_path)
             file_is_exists.__func__(file_name)
-            # print(f"wget {url} -O {local_path + file_name}")
-            # subprocess.call(f"wget {url} -O {local_path + file_name}", timeout=600)
-            wget.download(url, local_path + file_name)
+            subprocess.call(f"wget {url} -O {local_path + file_name}", timeout=600)
 
         except subprocess.TimeoutExpired as e:
             raise subprocess.SubprocessError("%s was terminated as of timeout!!" % e.cmd)
@@ -72,17 +58,18 @@ class HTTP:
             logger.info(f'{file_name} Success')
         finally:
             end = time.time()
-            timeusage = end - start
-            logger.info("Time usage: %.2f" % (end - start) + " second")
+            timeusage = end-start
+            logger.info("Time usage: %.2f" %(end - start) + " second")
         return timeusage
-
 
 if __name__ == '__main__':
     url = input("url：")
     local_path = input("local_path：")
     file_name = input("file_name：")
 
-    task = HTTP.download(url, local_path, file_name)
+    task = HTTP.DownLoad(url,local_path, file_name)
 
     # task = HTTP.DownLoad('https://smpdb.ca/downloads/smpdb_pathways.csv.zip',
     #                   '../../data_kk/smpdb/pathway/', '6')
+
+
