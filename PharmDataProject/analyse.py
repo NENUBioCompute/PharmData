0a01455b14ae5bfcf8b1f0474de599e6b664d65c
867844029 <140806462+867844029@users.noreply.github.com>
2024-03-11 17:47:05 +0800

    Add files via upload

---
+++ PharmDataProject/analyse.py
@@ -0,0 +1,32 @@
+import gzip
+import json
+
+class TransformJsonConverter:
+    def __init__(self, input_file, output_file):
+        self.input_file = input_file
+        self.output_file = output_file
+
+    def extract_gz_to_json(self):
+        with gzip.open(self.input_file, 'rb') as f_in:
+            content = f_in.read().decode('utf-8')
+
+        data = []
+        lines = content.split('\n')
+        header = lines[0].split('\t')
+        for line in lines[1:]:
+            if line:
+                values = line.split('\t')
+                entry = dict(zip(header, values))
+                data.append(entry)
+
+        with open(self.output_file, 'w') as f_out:
+            json.dump(data, f_out, indent=4)
+
+        print(f"文件 {self.output_file} 转换完成")
+
+# 创建类实例并调用方法
+input_file = '9606.protein_chemical.links.detailed.v5.0.tsv.gz'
+output_file = '9606.protein_chemical.links.detailed.v5.0.json'
+
+converter = TransformJsonConverter(input_file, output_file)
+converter.extract_gz_to_json()
---
+++ PharmDataProject/mongo.py
@@ -0,0 +1,32 @@
+import json
+from pymongo import MongoClient
+
+class JsonToMongoDB:
+    def __init__(self, json_file, mongodb_uri, db_name, collection_name):
+        self.json_file = json_file
+        self.mongodb_uri = mongodb_uri
+        self.db_name = db_name
+        self.collection_name = collection_name
+
+    def import_to_mongodb(self):
+        # 连接 MongoDB
+        client = MongoClient(self.mongodb_uri)
+        db = client[self.db_name]
+        collection = db[self.collection_name]
+
+        # 读取 JSON 文件数据
+        with open(self.json_file, 'r') as f:
+            data = json.load(f)
+
+        # 插入数据到集合中
+        result = collection.insert_many(data)
+        print(f"成功插入 {len(result.inserted_ids)} 条数据到 {self.collection_name} 集合")
+
+# 创建类实例并调用方法
+json_file = '9606.protein_chemical.links.detailed.v5.0.json'
+mongodb_uri = 'mongodb://localhost:27017/'
+db_name = 'mydatabase'
+collection_name = 'mycollection'
+
+json_to_mongodb = JsonToMongoDB(json_file, mongodb_uri, db_name, collection_name)
+json_to_mongodb.import_to_mongodb()
