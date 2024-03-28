a78a39fe6ff5b4f5c7e9a00270dfdc217e7f7ad8
whisper11111 <139330146+whisper11111@users.noreply.github.com>
2024-03-29 00:04:32 +0800

    Update BindingdbtoMongo.py

--- PharmDataProject/DatatoMongo/BindingdbtoMongo.py
+++ PharmDataProject/DatatoMongo/BindingdbtoMongo.py
@@ -1,86 +1,45 @@
-import os
 import pandas as pd
+import glob
 from pymongo import MongoClient
-from rdflib import Graph
-import zipfile
-import xml.etree.ElementTree as ET
-import libsbml
-import math
 
-# MongoDB 连接信息
 mongo_host = 'mongodb://readwrite:readwrite@59.73.198.168/?authMechanism=DEFAULT'
 mongo_port = 27017
 database_name = 'PharmRG'
+collection_name = 'FAERS'
 
-# 初始化 MongoDB 客户端
+
+
+# 连接 MongoDB
 client = MongoClient(mongo_host, mongo_port)
 db = client[database_name]
+collection = db[collection_name]
 
-def parse_biopax_and_store_to_mongo(directory_path, collection_name='SMPDB_BioPAX'):
-    """
-    解析BioPAX格式文件并存储到MongoDB。此处集成了BioPAX文件的自定义解析逻辑。
-    """
-    collection = db[collection_name]
-    for filename in os.listdir(directory_path):
-        if filename.endswith(".owl"):
-            file_path = os.path.join(directory_path, filename)
-            with open(file_path, 'r', encoding='utf-8') as file:
-                file_content = file.read()
+print("连接成功")
 
-            # 可能的文本处理逻辑
-            updated_content = re.sub(r'rdf:ID="([^"]*)"', lambda m: m.group(0).replace('/', '_').replace('-', '_').replace(',', '_'), file_content)
+# 获取所有文件的路径
+# file_paths = glob.glob("your_directory/*.csv")  # 替换为你存储FAERS数据库文件的文件夹路径
+file_paths = glob.glob("DEMO18Q2.csv") + glob.glob("DRUG18Q2.csv")+glob.glob("INDI18Q2.csv")+glob.glob("OUTC18Q2.csv")+glob.glob("REAC18Q2.csv")+glob.glob("RPSR18Q2.csv")+glob.glob("THER18Q2.csv")
 
-            # 使用rdflib处理更新后的内容
-            g = Graph()
-            g.parse(data=updated_content, format="xml")
-            for s, p, o in g:
-                triple = {'subject': str(s), 'predicate': str(p), 'object': str(o)}
-                collection.insert_one(triple)
-def parse_csv_and_store_to_mongo(zip_file_path, collection_name, csv_file_name=None):
-    """
-    解压缩CSV文件并存储到MongoDB。
-    """
-    if not csv_file_name:
-        csv_file_name = zip_file_path.split('/')[-1].replace('.zip', '.csv')
-    collection = db[collection_name]
-    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-        zip_ref.extractall(os.path.dirname(zip_file_path))
-    csv_file_path = os.path.join(os.path.dirname(zip_file_path), csv_file_name)
-    df = pd.read_csv(csv_file_path)
-    data_dict = df.to_dict(orient='records')
-    collection.insert_many(data_dict)
-    os.remove(csv_file_path)
+# 创建一个空的DataFrame来存储所有数据
+all_data = pd.DataFrame()
 
-def parse_xml_and_store_to_mongo(directory_path, collection_name, file_extension):
-    """
-    解析XML或SBGN文件并存储到MongoDB。
-    """
-    collection = db[collection_name]
-    for filename in os.listdir(directory_path):
-        if filename.endswith(file_extension):
-            file_path = os.path.join(directory_path, filename)
-            if file_extension == ".sbgn":
-                tree = ET.parse(file_path)
-                root = tree.getroot()
-                sbgn_data = {"file_path": file_path, "sbgn_elements": []}
-                for element in root.iter():
-                    element_data = {"tag": element.tag, "attributes": element.attrib}
-                    sbgn_data["sbgn_elements"].append(element_data)
-                collection.insert_one(sbgn_data)
+# 逐个读取文件并合并数据
+for file_path in file_paths:
+    data = pd.read_csv(file_path, low_memory=False)  # 读取文件
+    all_data = pd.concat([all_data, data], ignore_index=True)  # 连接数据
 
-def parse_sbml_and_store_to_mongo(directory_path, collection_name='SMPDB_SBML'):
-    """
-    解析SBML文件并存储到MongoDB。
-    """
-    collection = db[collection_name]
-    for filename in os.listdir(directory_path):
-        if filename.endswith(".sbml"):
-            sbml_file_path = os.path.join(directory_path, filename)
-            reader = libsbml.SBMLReader()
-            document = reader.readSBMLFromFile(sbml_file_path)
-            if document.getNumErrors() > 0:
-                continue
-            model = document.getModel()
-            if model:
-                model_info = {...} # 根据需求处理并收集模型信息
-                collection.insert_one(model_info)
+# 根据primaryid合并数据
+merged_data = all_data.groupby('primaryid').agg(lambda x: x.iloc[0])
+
+
+merged_data.fillna('', inplace=True)
+
+
+# 将合并后的数据转换为字典格式
+merged_data_dict = merged_data.to_dict(orient='index')
+
+# 将数据上传到MongoDB
+for primaryid, data_dict in merged_data_dict.items():
+    collection.update_one({'primaryid': primaryid}, {'$set': data_dict}, upsert=True)
+
+print("数据上传完成！")
