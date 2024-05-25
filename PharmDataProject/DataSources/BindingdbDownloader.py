381103fbed74a9a5faea1297d985e0d02f89a443
zhaojingtong1231 <2665109868@qq.com>
2024-05-25 12:01:20 +0800

    update

--- PharmDataProject/DataSources/BindingdbDownloader.py
+++ PharmDataProject/DataSources/BindingdbDownloader.py
@@ -1,4 +1,3 @@
-
 '''
 @Author : gaoqingshan
 @Time   : 2023/10/8 17:33
@@ -11,30 +10,42 @@ import io
 import zipfile
 import requests
 import logging
+from tqdm import tqdm
 
 config = configparser.ConfigParser()
-cfgfile = "../conf/drugkb.config"
+# cfgfile = "./conf/drugkb_test.config"
+cfgfile = r"D:\PharmData\PharmDataProject\conf\drugkb_test.config"
 config.read(cfgfile)
 
 
-def bindingdbDownloader():
-    url = args.filename
-
+def download_with_progress(url, dest_path):
     try:
         # Send the request and get the data
-        response = requests.get(url)
+        response = requests.get(url, stream=True)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
         logging.error("Request failed: %s", e)
         return
 
-    if response.status_code == 200:
-        with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
-            zip_ref.extractall(args.extract_dir)
+    total_size = int(response.headers.get('content-length', 0))
+    block_size = 1024  # 1 Kibibyte
+    t = tqdm(total=total_size, unit='iB', unit_scale=True)
+
+    with io.BytesIO() as file_stream:
+        for data in response.iter_content(block_size):
+            t.update(len(data))
+            file_stream.write(data)
+        t.close()
+
+        file_stream.seek(0)
+        with zipfile.ZipFile(file_stream) as zip_ref:
+            zip_ref.extractall(dest_path)
         logging.info("The TSV file is saved")
-        return
-    else:
-        logging.error("File download failure")
+
+
+def bindingdbDownloader():
+    url = args.source_url_1
+    download_with_progress(url, args.extract_dir)
 
 
 if __name__ == '__main__':
@@ -42,5 +53,5 @@ if __name__ == '__main__':
         description='downloadFromDB.py, This class downloads TSV files from the bindingdb database')
     parser.add_argument('source_url_1', help='Request address')
     parser.add_argument('extract_dir', help='Specify the decompression directory')
-    args = parser.parse_args([config.get('bindingdb', 'source_url_1'), '../data/bindingdb'])
+    args = parser.parse_args([config.get('bindingdb', 'source_url_1'), '../../data/bindingdb'])
     bindingdbDownloader()
