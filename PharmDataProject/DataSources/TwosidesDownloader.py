c2f47c123b824d2868090258a29fc1a31b9b8f66
zhaojingtong1231 <2665109868@qq.com>
2024-05-27 13:30:20 +0800

    update

--- PharmDataProject/DataSources/TwosidesDownloader.py
+++ PharmDataProject/DataSources/TwosidesDownloader.py
@@ -3,60 +3,53 @@ from urllib import request
 import os
 from tqdm import tqdm
 
+
+# ! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/15
+# @Author  : Yuanzhao Guo
+# @Email   : guoyz@nenu.edu.cn
+# @File    : Nsides.py
+# @Software: PyCharm
+
+
+from urllib import request
+import os
+import shutil
+from os.path import join, getsize
+import configparser
+
 class TwosidesDownloader:
     def __init__(self):
-        pass
+        self.config = configparser.ConfigParser()
+        self.cfgfile = '../conf/drugkb_test.config'
+        self.config.read(self.cfgfile)
+    def download(self,url, dir):
+        os.makedirs(os.path.dirname(dir), exist_ok=True)
+        print("downloading with urllib")
+        request.urlretrieve(url, dir)
 
-    def download(self, url, dir):
-        print(f"Downloading from {url} to {dir}")
-        with tqdm(unit='B', unit_scale=True, miniters=1, desc=os.path.basename(dir)) as t:
-            request.urlretrieve(url, dir, reporthook=self.report_hook(t))
-        print(f"Downloaded to {dir}")
+    def get_twosides(self,url, dir):
+        dir1 = dir + "TWOSIDES.csv.gz"
+        self.download(url, dir1)
+        print('下载完成')
 
-    def report_hook(self, t):
-        last_b = [0]
+    def get_offsides(self,url, dir):
+        dir2 = dir + "OFFSIDES.csv.gz"
+        self.download(url, dir2)
+        print('下载完成')
+    def download_all(self):
+        self.get_twosides(self.config.get('twosides', 'source_url_1'),
+                     self.config.get('twosides', 'data_path_1'))
 
-        def update_to(b=1, bsize=1, tsize=None):
-            if tsize is not None:
-                t.total = tsize
-            t.update((b - last_b[0]) * bsize)
-            last_b[0] = b
-        return update_to
+        self.get_offsides(self.config.get('twosides', 'source_url_2'),
+                     self.config.get('twosides', 'data_path_2'))
 
-    def get_twosides(self, url, dir):
-        dir1 = os.path.join(dir, "TWOSIDES.csv.gz")
-        print(f"Checking if TWOSIDES needs to be downloaded to {dir1}")
-        if not os.path.exists(dir1):
-            self.download(url, dir1)
-            print('TWOSIDES 下载完成')
-        else:
-            print(f"{dir1} already exists, skipping download.")
-
-    def get_offsides(self, url, dir):
-        dir2 = os.path.join(dir, "OFFSIDES.csv.gz")
-        print(f"Checking if OFFSIDES needs to be downloaded to {dir2}")
-        if not os.path.exists(dir2):
-            self.download(url, dir2)
-            print('OFFSIDES 下载完成')
-        else:
-            print(f"{dir2} already exists, skipping download.")
 
 if __name__ == '__main__':
-    config = configparser.ConfigParser()
-    cfgfile = '../conf/drugkb_test.config'
-    config.read(cfgfile)
-    downloader = TwosidesDownloader()  # 创建 TwosidesDownloader 类的实例
+    twosidesdownloader = TwosidesDownloader()
+    twosidesdownloader.download_all()
 
-    # 使用实例调用方法
-    twosides_url = config.get('twosides', 'source_url_1')
-    twosides_path = config.get('twosides', 'data_path_2')
-    offsides_url = config.get('twosides', 'source_url_2')
-    offsides_path = config.get('twosides', 'data_path_1')
 
-    print(f"TWOSIDES download URL: {twosides_url}")
-    print(f"TWOSIDES save path: {twosides_path}")
-    downloader.get_twosides(twosides_url, twosides_path)
 
-    print(f"OFFSIDES download URL: {offsides_url}")
-    print(f"OFFSIDES save path: {offsides_path}")
-    downloader.get_offsides(offsides_url, offsides_path)
+
