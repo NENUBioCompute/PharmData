f996d71590a85e776d028db539052a765a56b0c9
zhaojingtong1231 <2665109868@qq.com>
2024-05-27 11:55:54 +0800

    update

--- PharmDataProject/DataSources/StitchDownloader.py
+++ PharmDataProject/DataSources/StitchDownloader.py
@@ -1,54 +1,16 @@
-# -*- encoding: utf-8 -*-
-# @Author: Deepwind
-# @Time  : 4/15/2024 11:04 PM
-# @Email: deepwind32@163.com
-
+"""
+  -*- encoding: utf-8 -*-
+  @Author: Deepwind
+  @Time  : 4/15/2024 11:04 PM
+  @Email: deepwind32@163.com
+"""
 import os
 import subprocess
 import threading
-import requests
-from tqdm import tqdm
+
+from DownloadKit import DownloadKit
+
 from PharmDataProject.Utilities.FileDealers.ConfigParser import ConfigParser
-
-
-class DownloadKit:
-    def __init__(self, data_path):
-        self.data_path = data_path
-
-    def add(self, url, rename=None):
-        self.url = url
-        self.filename = rename if rename else url.split('/')[-1]
-        return self
-
-    def wait(self, show=True):
-        retries = 5
-        for _ in range(retries):
-            try:
-                response = requests.get(self.url, stream=True)
-                response.raise_for_status()
-                total_size = int(response.headers.get('content-length', 0))
-                filepath = os.path.join(self.data_path, self.filename)
-
-                if os.path.exists(filepath):
-                    print(f"{filepath} already exists. Skipping download.")
-                    return self
-
-                with open(filepath, 'wb') as file, tqdm(
-                        desc=filepath,
-                        total=total_size,
-                        unit='B',
-                        unit_scale=True,
-                        unit_divisor=1024,
-                ) as bar:
-                    for chunk in response.iter_content(chunk_size=8192):
-                        file.write(chunk)
-                        bar.update(len(chunk))
-
-                print(f"{self.filename} downloaded successfully.")
-                return self
-            except requests.exceptions.RequestException as e:
-                print(f"Download failed: {e}. Retrying...")
-        raise Exception(f"Failed to download {self.filename} after {retries} retries.")
 
 
 class StitchDownloader:
@@ -59,23 +21,14 @@ class StitchDownloader:
 
     def __download_and_gunzip(self, filename, url):
         self.DownloadKit.add(url, rename=filename).wait(show=False)
-        filepath = os.path.join(self.data_path, filename)
-        if os.path.exists(filepath):
-            subprocess.run(["gunzip", "-f", filepath])
-            if not os.path.exists(filepath):
-                print(f"{filename} downloaded and unzipped successfully.")
-            else:
-                print(f"Failed to unzip {filename}.")
-        else:
-            print(f"{filename} does not exist, cannot unzip.")
+        subprocess.run(["gunzip", os.path.join(self.data_path, filename)])
+        # print(f"{filename} download successfully.")
 
     def start(self):
-        urls = [
-            (self.config.get("cc_links_filename"), self.config.get("cc_links_url")),
-            (self.config.get("pc_links_detailed_filename"), self.config.get("pc_links_detailed_url")),
-            (self.config.get("pc_links_transfer_filename"), self.config.get("pc_links_transfer_url")),
-            (self.config.get("actions_filename"), self.config.get("actions_url"))
-        ]
+        urls = [(self.config.get("cc_links_filename"), self.config.get("cc_links_url")),
+                (self.config.get("pc_links_detailed_filename"), self.config.get("pc_links_detailed_url")),
+                (self.config.get("pc_links_transfer_filename"), self.config.get("pc_links_transfer_url")),
+                (self.config.get("actions_filename"), self.config.get("actions_url"))]
 
         threads = [threading.Thread(target=self.__download_and_gunzip, args=(filename, url)) for filename, url in urls]
         for thread in threads:
