31e815f5cc20cbb0b90100ede11724b1421776a7
zhaojingtong1231 <2665109868@qq.com>
2024-05-28 20:15:27 +0800

    update

--- PharmDataProject/DataParsers/CheMBLParser.py
+++ PharmDataProject/DataParsers/CheMBLParser.py
@@ -5,17 +5,3 @@
   @Email: 2665109868@qq.com
   @function
 """
-import pymysql
-import pandas as pd
-
-def get_mysql_conn():
-    # 设置数据库连接参数
-    db_config = {
-        'host': '59.73.198.168',
-        'user': 'root',
-        'password': '001231',
-        'database': 'chembl_33',
-    }
-    # 创建连接
-    conn = pymysql.connect(**db_config)
-    return conn
