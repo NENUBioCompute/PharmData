470208109971bf3ec3a13e4de1d00285f6d6e1b5
cuichen <1197956390@qq.com>
2024-05-29 18:10:06 +0800

    Updated TwosidesToMongo, config file, and TwosidesParser

--- PharmDataProject/DataParsers/TwosidesParser.py
+++ PharmDataProject/DataParsers/TwosidesParser.py
@@ -1,15 +1,6 @@
-"""
-  -*- encoding: utf-8 -*-
-  @Author: zhuliujinxiang
-  @Time  : 2023/10/10 21:53
-  @Email: deepwind32@163.com
-  @function
-"""
+# twosides_parser.py
 import logging
 import csv
-import configparser
-from PharmDataProject.Utilities.Database.dbutils import DBconnection
-
 
 class TwosidesParser:
     def __init__(self, csv_path: str, all_field=True):
@@ -77,28 +68,3 @@ class TwosidesParser:
             for row in db_reader:
                 drug = dict(zip(header, [row[i] for i in field_allowed]))
                 yield drug
-
-
-if __name__ == "__main__":
-
-    config = configparser.ConfigParser()
-    cfgfile = '../conf/drugkb.config'
-    config.read(cfgfile)
-
-    for i in range(0, int(config.get('twosides', 'data_path_num'))):
-        db = DBconnection(cfgfile, config.get('twosides', 'db_name'),
-                          config.get('twosides', 'col_name_' + str(i + 1)))
-
-        data_path = config.get('twosides', 'data_path_' + str(i + 1))
-
-        # logging.basicConfig(level=logging.INFO,
-        #                     format='%(asctime)s %(levelname)s %(message)s',
-        #                     datefmt='%m/%d %I:%M:%S')
-        iter_drug = TwosidesParser(data_path).parse()
-        for index, item in enumerate(iter_drug):
-            db.collection.insert_one(item)
-            if index % 10000 == 0:
-                print(index)
-            # if index == 5:
-            #     break
-            # print(item)
