0615f12ddad0f94f6f7350cf3ddd0d2f026f8673
zhaojingtong1231 <2665109868@qq.com>
2024-05-29 11:53:38 +0800

    update

--- PharmDataProject/DatatoMongo/WikipathwayToMongo.py
+++ PharmDataProject/DatatoMongo/WikipathwayToMongo.py
@@ -7,37 +7,35 @@ from PharmDataProject.Utilities.Database.dbutils import DBconnection
 from PharmDataProject.DataParsers.WikipathwayParser import WikipathwayParser
 
 
-class WikipathwayDBInserter:
-    def __init__(self, config, section_name):
-        self.dbc = DBconnection('../conf/drugkb_test.config', config.get(section_name, 'db_name'),
-                                config.get(section_name, 'col_name_1'))
+class WikipathwaytoMongo:
+    def __init__(self, cfgfile):
+        self.config = configparser.ConfigParser()
+        self.cfgfile = cfgfile
+        self.config.read(cfgfile)
+        self.name = 'wikipathway'
+        self.db = DBconnection(self.cfgfile, self.config.get(self.name, 'db_name'),
+                                self.config.get(self.name, 'col_name_1'))
 
     def insert_pathway(self, pathway):
-        self.dbc.collection.insert_one(pathway)
-        return 1
-
+        self.db.collection.insert_one(pathway)
     def insert_all(self, pathways):
         for pathway in pathways:
             self.insert_pathway(pathway)
+    def save_to_mongo(self):
+        parser = WikipathwayParser(self.config, self.name)
+        infile = os.path.join(self.config.get(self.name, 'data_path_1'),
+                              self.config.get(self.name, 'source_url_1').split('/')[-1])
+        pathways = parser.read_and_index_pathways(infile)
+
+        try:
+            print('begin')
+            self.insert_all(pathways)
+
+        except StopIteration:
+            print("No pathways found in the file.")
 
 
 if __name__ == '__main__':
-
-    config = configparser.ConfigParser()
     cfgfile = '../conf/drugkb_test.config'
-    config.read(cfgfile)
-
-    parser = WikipathwayParser(config, 'wikipathway')
-    inserter = WikipathwayDBInserter(config, 'wikipathway')
-
-    infile = os.path.join(config.get('wikipathway', 'data_path_1'), 'wikipathways-20231010-gpml-Homo_sapiens.zip')
-    pathways = parser.read_and_index_pathways(infile)
-
-    # Insert the first dictionary for testing
-    try:
-        first_pathway = next(pathways)
-        print("First pathway parsed:", first_pathway)
-        inserter.insert_pathway(first_pathway)
-        print("First pathway inserted into the database")
-    except StopIteration:
-        print("No pathways found in the file.")
+    wikipathwaytomongo = WikipathwaytoMongo(cfgfile)
+    wikipathwaytomongo.save_to_mongo()
