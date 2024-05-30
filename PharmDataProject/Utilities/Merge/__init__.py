054053c1007ed08071fc1e653cdc292469a162c4
Deepwind64 <deepwind32@163.com>
2024-05-30 21:01:50 +0800

    ttd merge done

---
+++ PharmDataProject/Utilities/Merge/__init__.py
@@ -0,0 +1,6 @@
+"""
+  -*- encoding: utf-8 -*-
+  @Author: Deepwind
+  @Time  : 5/27/2024 2:16 PM
+  @Email: deepwind32@163.com
+"""
--- PharmDataProject/merge_codes/merge_drug.py
+++ PharmDataProject/merge_codes/merge_drug.py
@@ -5,6 +5,7 @@
   @Email: deepwind32@163.com
 """
 import json
+import pickle
 import threading
 import time
 from queue import Queue
@@ -15,103 +16,7 @@ from tqdm import tqdm
 
 from PharmDataProject.Utilities.Database.dbutils_v2 import DBConnection
 from PharmDataProject.Utilities.FileDealers.ConfigParser import ConfigParser
-
-
-class MergeUtils:
-    @staticmethod
-    def merge_with_pubchem(thread_id, id_queue, id_mapping, share_counter, locks, not_found_list, bar=None):
-        """
-        use with Thread, fetch pubchem cid and sid with giving info
-        :param thread_id: an identifier
-        :param id_queue: (id, info)
-        :param id_mapping: {id: (cid, sid)}
-        :param share_counter: a counter shared with threads
-        :param locks: locks
-        :param not_found_list: to output the not found id
-        """
-        share_counter.setdefault("inner_count", {})[thread_id] = 0
-
-        while not id_queue.empty():
-            share_counter["inner_count"][thread_id] += 1
-            execute_flag = True
-            max_retry = 3
-            id, info = id_queue.get()
-
-            while execute_flag and max_retry:
-                max_retry -= 1
-                pubchem_cid, pubchem_sid = None, None
-                try:
-                    # archive cid by info
-                    try:
-                        pubchem_cid = pcp.get_compounds(f"{info}", "name")[0].cid
-                        with locks["cid_lock"]:
-                            share_counter["cid_count"] += 1
-                    except (pcp.BadRequestError, IndexError):
-                        pass
-
-                    # archive sid by info
-                    try:
-                        pubchem_sid = [i.sid for i in pcp.get_substances(f"{info}", "name")]
-                        with locks["sid_lock"]:
-                            share_counter["sid_count"] += 1
-                    except pcp.BadRequestError:
-                        pass
-
-                    # above code running successfully, stop next iter
-                    execute_flag = False
-
-                    # add into list
-                    with locks["save_lock"]:
-                        if pubchem_cid or pubchem_sid:
-                            id_mapping[id] = {"cid": pubchem_cid, "sid": pubchem_sid}
-                        else:
-                            not_found_list.append(id)
-                            share_counter["not_found_count"] += 1
-
-                except Exception as e:
-                    print(id, e)
-                    time.sleep(3)
-            if bar is not None:
-                bar.update(1)
-            if max_retry < 0:
-                raise Exception(f"Achieve id {id} failed")
-        return id_mapping
-
-    @staticmethod
-    def merge_id_record(db, pubchem_id_mapping, merge_dbname):
-        insert_list = []
-        for id, value in tqdm(pubchem_id_mapping.items(), desc="updating database"):
-            target = None
-            # merge according cid
-            if value.get("cid", None):
-                target = db.find_one({"pubchem.cid": value["cid"]})
-            else:
-                # merge according sid
-                for sid in value.get("sid", []):
-                    target = db.find_one({"pubchem.sid": sid})
-                    if target:
-                        break
-
-            if target:
-                all_sid = target["pubchem"]["sid"]
-                all_sid.extend(value.get("sid", []))
-                query = {"_id": target["_id"]}
-                update_operation = {
-                    '$set': {
-                        "pubchem": {"cid": target["pubchem"]["cid"], "sid": list(set(all_sid))},
-                        merge_dbname: {"_id": id}
-                    }
-                }
-                db.update_one(query, update_operation)
-            else:
-                insert_list.append({
-                    "pubchem": {
-                        "cid": value.get("cid", None),
-                        "sid": value.get("sid", None)
-                    },
-                    merge_dbname: {"_id": id}
-                })
-        db.insert(insert_list)
+from PharmDataProject.Utilities.Merge.MergeUtils import MergeUtils
 
 
 class MergeDrug:
@@ -121,87 +26,6 @@ class MergeDrug:
                                config.get("collection_name", "merge_drugs"),
                                config=config, empty_check=False)
         self.share_var = {}
-
-    def merge_ttd(self):
-        # init var
-        share_var = {
-            "all_count": 0,
-            "inchi_count": 0,
-            "smiles_count": 0,
-            "name_count": 0,
-            "not_found_count": 0,
-        }
-        inner_var = {
-            "all_count": 0,
-            "cid_count": 0,
-            "sid_count": 0,
-            "not_found_count": 0,
-        }
-
-        id_queue = Queue()
-        locks = {"all_lock": threading.Lock(),
-                 "cid_lock": threading.Lock(),
-                 "sid_lock": threading.Lock(),
-                 "save_lock": threading.Lock()}
-
-        not_found = []
-
-        # get id mapping from ttd_crossmatching
-        pubchem_id_mapping, name_list = self.merge_drug_cm()
-        for item in name_list:
-            id_queue.put(item)
-
-        # collect info and put into id_queue
-        for i in tqdm(self.db.get_iter(self.config.get("col_name_2", "ttd")), desc="reading ttd_drug"):
-            share_var["all_count"] += 1
-
-            id = i.get("DRUG__ID").get("value")
-            if id in pubchem_id_mapping.keys():
-                continue
-
-            inchi_key = i.get("DRUGINKE")
-            if inchi_key and inchi_key.get("value"):
-                share_var["inchi_count"] += 1
-                id_queue.put((id, inchi_key.get("value")))
-                continue
-
-            smiles = i.get("DRUGSMIL")
-            if smiles and smiles.get("value"):
-                share_var["smiles_count"] += 1
-                id_queue.put((id, smiles.get("value")))
-                continue
-
-            name = i.get("TRADNAME")
-            if name and name.get("value"):
-                share_var["name_count"] += 1
-                id_queue.put((id, name.get("value")))
-                continue
-
-            share_var["not_found_count"] += 1
-
-        # search pubchem id with giving info
-        id_mapping = {}
-        bar = tqdm(desc="searching pubchem")
-        threads = [Thread(target=MergeUtils.merge_with_pubchem, args=
-        (i, id_queue, id_mapping, inner_var, locks, not_found, bar))
-                   for i in range(3)]
-        for thread in threads:
-            thread.start()
-        for thread in threads:
-            thread.join()
-        bar.close()
-
-        # if some id don't find corresponding pubchem id
-        if not_found:
-            print(f"{len(not_found)} ids can not be found.")
-            with open("ttd_cm_not_found_id.txt", "w") as f:
-                json.dump(not_found, f, indent=4)
-
-        pubchem_id_mapping.update(id_mapping)
-        print("ttd_drug_count", share_var, sep="\n")
-        print("ttd_drug_cm_count", inner_var, sep="\n")
-        print(f"found: {share_var['all_count'] - share_var['not_found_count'] - inner_var['not_found_count']}")
-        MergeUtils.merge_id_record(self.db, pubchem_id_mapping, "ttd")
 
     def merge_drugbank(self):
         self.share_var = {
@@ -333,8 +157,86 @@ class MergeDrug:
 
         print("drugbank finish merging")
 
-    def start(self):
-        pass
+    def merge_ttd(self):
+        # init var
+        share_var = {
+            "all_count": 0,
+            "inchi_count": 0,
+            "smiles_count": 0,
+            "name_count": 0,
+            "not_found_count": 0,
+        }
+        inner_var = {
+            "all_count": 0,
+            "cid_count": 0,
+            "sid_count": 0,
+            "not_found_count": 0,
+        }
+
+        id_queue = Queue()
+        locks = {"all_lock": threading.Lock(),
+                 "cid_lock": threading.Lock(),
+                 "sid_lock": threading.Lock(),
+                 "save_lock": threading.Lock()}
+
+        not_found = []
+
+        # get id mapping from ttd_crossmatching
+        pubchem_id_mapping, name_list = self.merge_drug_cm()
+        for item in name_list:
+            id_queue.put(item)
+
+        # collect info and put into id_queue
+        for i in tqdm(self.db.get_iter(self.config.get("col_name_2", "ttd")), desc="reading ttd_drug"):
+            share_var["all_count"] += 1
+
+            id = i.get("DRUG__ID").get("value")
+            if id in pubchem_id_mapping.keys():
+                continue
+
+            inchi_key = i.get("DRUGINKE")
+            if inchi_key and inchi_key.get("value"):
+                share_var["inchi_count"] += 1
+                id_queue.put((id, inchi_key.get("value")))
+                continue
+
+            smiles = i.get("DRUGSMIL")
+            if smiles and smiles.get("value"):
+                share_var["smiles_count"] += 1
+                id_queue.put((id, smiles.get("value")))
+                continue
+
+            name = i.get("TRADNAME")
+            if name and name.get("value"):
+                share_var["name_count"] += 1
+                id_queue.put((id, name.get("value")))
+                continue
+
+            share_var["not_found_count"] += 1
+
+        # search pubchem id with giving info
+        id_mapping = {}
+        bar = tqdm(desc="searching pubchem")
+        threads = [Thread(target=MergeUtils.merge_with_pubchem, args=
+        (i, id_queue, id_mapping, inner_var, locks, not_found, bar))
+                   for i in range(3)]
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+        bar.close()
+
+        # if some id don't find corresponding pubchem id
+        if not_found:
+            print(f"{len(not_found)} ids can not be found.")
+            with open("ttd_cm_not_found_id.txt", "w") as f:
+                json.dump(not_found, f, indent=4)
+
+        pubchem_id_mapping.update(id_mapping)
+        print("ttd_drug_count", share_var, sep="\n")
+        print("ttd_drug_cm_count", inner_var, sep="\n")
+        print(f"found: {share_var['all_count'] - share_var['not_found_count'] - inner_var['not_found_count']}")
+        MergeUtils.merge_id_record(self.db, pubchem_id_mapping, "ttd")
 
     def merge_drug_cm(self):
         pubchem_id_mapping = {}
@@ -363,11 +265,142 @@ class MergeDrug:
 
         return pubchem_id_mapping, name_list
 
+    def merge_kegg(self):
+        ids = self._divide_id_kegg()
+        self._search_id_kegg(ids)
+
+    def _divide_id_kegg(self):
+        def find_and_split(name):
+            if str_flag:
+                return db_links.split()[1]
+            else:
+                for i in db_links:
+                    if name in i:
+                        return i.split()[1]
+
+        counter = {
+            "all_count": 0,
+            "pubchem_count": 0,
+            "cas_count": 0,
+            "chebl_count": 0,
+            "other_count": 0,
+            "no_link_db": 0
+        }
+        ids = {
+            "cas": [],
+            "pubchem": [],
+            "chebl": []
+        }
+        other_set = set()
+        not_found_list = []
+        for i in tqdm(self.db.get_iter(self.config.get("col_name_1", "kegg")), desc="reading kegg_drugs"):
+            counter["all_count"] += 1
+            str_flag = False
+            db_links = i.get("DBLINKS")
+            if db_links:
+                if type(db_links) is str:
+                    str_flag = True
+                    s = db_links
+                else:
+                    s = " ".join(db_links)
+                if "PubChem" in s:
+                    counter["pubchem_count"] += 1
+                    ids["pubchem"].append((i.get("ENTRY"), find_and_split("PubChem")))
+                    continue
+                elif "CAS" in s:
+                    counter["cas_count"] += 1
+                    # find and split "CAS: 101-31-5"
+                    ids["cas"].append((i.get("ENTRY"), find_and_split("CAS")))
+                    continue
+                elif "ChEBI" in s:
+                    counter["chebl_count"] += 1
+                    ids["chebl"].append((i.get("ENTRY"), find_and_split("ChEBI")))
+                    continue
+                else:
+                    counter["other_count"] += 1
+                    if str_flag:
+                        other_set.add(s.split(":")[0])
+                    else:
+                        for i in db_links:
+                            other_set.add(i.split(":")[0])
+
+            else:
+                counter["no_link_db"] += 1
+                not_found_list.append(i.get("ENTRY"))
+
+        with open("kegg_drug_not_found.txt", "w") as f:
+            f.write("source_kegg_drugs, 'ENTRY'")
+            f.writelines(not_found_list)
+            print("successfully save not found record 'ENTRY'")
+        print(counter)
+        print(other_set)
+
+        return ids
+
+    def _search_id_kegg(self, ids):
+        inner_var = {
+            "all_count": 0,
+            "cid_count": 0,
+            "sid_count": 0,
+            "not_found_count": 0,
+        }
+
+        sid2cid_queue, name_queue = Queue(), Queue()
+        locks = {"all_lock": threading.Lock(),
+                 "cid_lock": threading.Lock(),
+                 "sid_lock": threading.Lock(),
+                 "save_lock": threading.Lock()}
+
+        not_found = []
+        # using pubchem sid to find cid
+        for item in ids["pubchem"]:
+            sid2cid_queue.put(item)
+
+        for item in ids["cas"]:
+            name_queue.put(item)
+        for item in ids["chebl"]:
+            name_queue.put(item)
+
+        # search pubchem id with giving info
+        id_mapping = {}
+
+        bar = tqdm(desc="searching pubchem sid2cid")
+        threads_sid = [Thread(target=MergeUtils.merge_with_pubchem, args=
+        (i, sid2cid_queue, id_mapping, inner_var, locks, not_found, bar, "sid2cid"))
+                       for i in range((int(self.config.get("thread_num"))))]
+        for thread in threads_sid:
+            thread.start()
+        for thread in threads_sid:
+            thread.join()
+        bar.close()
+        print(inner_var)
+        print(not_found, end="\n\n")
+
+        bar = tqdm(desc="searching pubchem with name")
+        threads_name = [Thread(target=MergeUtils.merge_with_pubchem, args=
+        (i, name_queue, id_mapping, inner_var, locks, not_found, bar, "name"))
+                        for i in range((int(self.config.get("thread_num"))))]
+        for thread in threads_name:
+            thread.start()
+        for thread in threads_name:
+            thread.join()
+        bar.close()
+
+        print(inner_var)
+        print(not_found)
+
+        with open("id_mapping.pkl", "wb") as f:
+            pickle.dump(id_mapping, f)
+        MergeUtils.merge_id_record(self.db, id_mapping, "kegg")
+
+    def start(self):
+        # self.merge_drugbank()
+        # self.merge_ttd()
+        self.merge_kegg()
+
 
 if __name__ == "__main__":
     cfg = "/home/zhaojingtong/tmpcode/PharmData/PharmDataProject/conf/drugkb.config"
     config = ConfigParser(cfg, "merge_drugs")
     md = MergeDrug(config)
-    # assume collection merge_drugs is empty or a exception will be raised
-    # md.merge_drugbank()
-    md.merge_ttd()
+    md.start()
